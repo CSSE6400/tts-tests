64232d21531a0df59e7d7a98f953f08eeaabdac2
Brae Webb <email@braewebb.com>
2022-05-03 17:54:10 +1000

    Enable monitoring dashboard

--- run.sh
+++ run.sh
@@ -7,6 +7,12 @@ if [ $# -eq 0 ]
 fi
 
 export ENDPOINT=$1
+# if -l flag is set, then log to influxdb
+if [ "$2" = "-l" ]
+  then
+      export K6_OUT=influxdb=http://localhost:8086/k6
+fi
+
 
 k6 run ./api-conformance.js
 k6 run ./processing.js
